0d4ab447006925715316107b7a6ed87632087dbd
ivan.khvostov <i.khvostov@smartdata.dev>
2024-09-30 08:39:29 +1000

    Удаление лишних комменетариев

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,5 +1,3 @@
--- psql -U your_db_user -d menu_db -f sql/schema.sql
-
 CREATE TABLE categories (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -7,5 +5,4 @@ CREATE TABLE categories (
     parent_id INTEGER REFERENCES categories(id) ON DELETE CASCADE
 );
 
--- Индекс для быстрого поиска по parent_id
 CREATE INDEX idx_parent_id ON categories(parent_id);
